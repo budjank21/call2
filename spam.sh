9455a2e0ce36ad14598b5169b0cce94200b403ed
BudjankGaming <92497066+budjank21@users.noreply.github.com>
2021-11-02 19:37:05 +0700

    Add files via upload

---
+++ spam.sh
@@ -0,0 +1,98 @@
+#!/bin/sh/spam-call
+# by andi ya gays ya
+
+
+
+# warana
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+# tampilan
+clear
+sleep 1
+echo
+toilet CALLSAY | lolcat
+sleep 1
+echo $white"           CUMA TESTING SAJA"
+echo $red"==============================================="
+echo $white" Author"$red" :"$white" Andi Andrian"
+echo $white" Channel"$red"   :"$white" Budjank Channel"
+echo $white" Kontak"$red" :"$white" +62 82121033609"
+echo $red"==============================================="
+echo
+sleep 1
+echo $cyan"         INGAT GUNAKAN DENGAN BIJAK"
+echo $red"        ############################"
+echo
+   # daftar menu
+   sleep 1
+   echo $cyan"[0]"$white" Keluar Saja Takut Dosa"
+   echo $red"------------------------------------------"
+   echo $cyan"[1]"$white" Spam Call By Andi Andrian"
+   echo $red"------------------------------------------"
+   echo $cyan"[2]"$white" Spam Call By Abilseno"
+   echo $red"------------------------------------------"
+   echo $cyan"[3]"$white" Spam Call By Hack Bae"
+   echo $red"------------------------------------------"
+   echo $white
+   read -p "pilih >> " gay
+
+# data 0
+if [ $gay = 0 ]; then
+figlet babay | lolcat
+exit
+fi
+
+# data 1
+if [ $gay = 1 ]; then
+echo "=> install github" | lolcat
+sleep 1
+git clone https://github.com/MrVirusSpm-07/call-01
+sleep 1
+echo "=> install cd" | lolcat
+sleep 2
+cd call-01
+echo "=> install selesai" | lolcat
+bash callv1.sh
+fi
+
+
+# data 2
+if [ $gay = 3 ]; then
+echo "=> install github" | lolcat
+sleep 1
+git clone https://github.com/zlucifer/troll_project
+sleep 1
+echo "=> install cd" | lolcat
+sleep 2
+cd troll_project
+sleep 1
+echo "=> instal selesai" | lolcat
+sleep 2
+sh troll.sh
+fi
+
+
+# data 3
+if [ $gay = 2 ]; then
+echo "=> install github" | lolcat
+sleep 1
+git clone https://github.com/AbilSeno/MDSpammers
+sleep 1
+echo "=> install cd" | lolcat
+sleep 1
+cd MDSpammers
+sleep 1
+echo "=> install requests" | lolcat
+sleep 1
+pip2 install requests
+sleep 1
+echo "=> install selesai" | lolcat
+sleep 1
+python2 main.py
+fi
